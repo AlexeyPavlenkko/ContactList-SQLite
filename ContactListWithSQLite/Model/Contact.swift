90c5b10a1c0f5e1f55a0f6536d6ab913616efcf5
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-07 17:05:21 +0300

    added contact model

---
+++ ContactListWithSQLite/Model/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  ContactListWithSQLite
+//
+//  Created by Алексей Павленко on 06.09.2022.
+//
+
+import Foundation
+
+struct Contact {
+    
+    //VAriables
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let phoneNumber: String
+    let photo: Data
+}
