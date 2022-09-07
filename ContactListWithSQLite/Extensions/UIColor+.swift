7a034d02ea27053186010694557e54a77fb45b9e
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-07 17:06:15 +0300

    extension for getting main color fast and easy

---
+++ ContactListWithSQLite/Extensions/UIColor+.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor.swift
+//  ContactListWithSQLite
+//
+//  Created by Алексей Павленко on 06.09.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let main = UIColor(named: "main")!
+}
